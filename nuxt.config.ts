d3914e3fdf75b77398454fa5ac4d71e540c6acad
daunload <daun4799@gmail.com>
2025-08-26 19:16:09 +0900

    timepicker 컴포넌트를 사용하기 위해 shadcn-vue 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,21 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	modules: ['@nuxt/eslint', '@nuxt/ui'],
+	modules: ['@nuxt/eslint', '@nuxt/ui', '@vueuse/nuxt', 'shadcn-nuxt'],
 	css: ['~/assets/css/main.css'],
 	runtimeConfig: {
 		DB_URL: process.env.DB_URL,
 		DB_NAME: process.env.DB_NAME,
+	},
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: '',
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: './components/ui',
 	},
 });
